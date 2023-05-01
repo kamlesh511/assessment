90d4d7402a7be8c0923d097aaf5ffb2ea34dc994
kamlesh511 <112157747+kamlesh511@users.noreply.github.com>
2023-05-01 12:49:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+$choice =1;
+print("Welcome to library manegment system ");
+print(" select ur role \n");
+print("1. Add books\n");
+print("2. delete books\n");
+print("3. search books\n");
+print("4. view list\n");
+print("5. Edit books record\n");
+print("6. change password\n");
+print("7. close application\n");
+
+$choice =readline("enter ur role\n ");
+if($choice==1)
+{    
+
+    print("1. add books\n");
+}
+
+$choice1=readline(".select  books\n");
+{
+    print("1. computer\n");
+    print("2. electrical\n");
+    print("3. civil\n");
+    print("4. mechanical\n");
+    print("5. architecture\n");
+    print("6. electronic\n");
+    print("7. back to main menu\n");
+}
+$choice =readline ("Enter the information Below\n");
+if($choice==2)
+{
+    print("category:-computer\n");
+    
+}
+
+
+
+
+
+
+?>
